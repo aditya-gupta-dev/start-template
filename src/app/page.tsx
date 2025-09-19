427a5d8495503089252062ba4900c2a9520acdcf
aditya-gupta-dev <adityagupta2087@gmail.com>
2025-09-20 01:02:26 +0530

    added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <ModeToggle/>
+      <SignedIn>
+        <ModeToggle />
+        <SignOutButton>
+          <Button>Sign out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>
+          <Button>
+            <h1>Sign in</h1>
+          </Button>
+        </SignInButton>
+      </SignedOut>
     </>
   );
 }
