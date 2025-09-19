cf48dfaecdc25da8419d1ca1264e18bd965b7a44
aditya-gupta-dev <adityagupta2087@gmail.com>
2025-09-19 16:38:13 +0530

    added mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-    </div>
+    <>
+      <ModeToggle/>
+    </>
   );
 }
